82f443f72ef949f9c1de60446806661a5c03ef1b
Cristian Rosa <cristian@bravewealth.com>
2025-05-30 10:44:33 -0300

    05 - Ticket v2

--- exercises/05_ticket_v2/14_source/src/lib.rs
+++ exercises/05_ticket_v2/14_source/src/lib.rs
@@ -23,6 +23,8 @@ pub enum TicketNewError {
     DescriptionCannotBeEmpty,
     #[error("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
+    #[error("{0}")]
+    InvalidStatus(#[from] status::ParseStatusError),
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -47,7 +49,7 @@ impl Ticket {
             return Err(TicketNewError::DescriptionTooLong);
         }
 
-        // TODO: Parse the status string into a `Status` enum.
+        let status = Status::try_from(status)?;
 
         Ok(Ticket {
             title,
